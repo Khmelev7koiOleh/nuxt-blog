184d584f410be9c73c3371febdbdb780cd8e6451
Oleh Khmelevskoi <khmelevskoioleh@gmail.com>
2024-11-22 12:46:34 +0100

    first commit

---
+++ components/layout/Sidebar.vue
@@ -0,0 +1,11 @@
+<template>
+  <aside class="flex flex-col items-center">
+    <NuxtLink to="/">
+      <NuxtImg src="/icon-green.png" width="100" />
+    </NuxtLink>
+  </aside>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
